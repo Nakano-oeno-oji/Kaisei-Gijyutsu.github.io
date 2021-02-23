a3180e0c23bcdde9f756df21b2ef5021a90856ec
Nakano-oeno-oji <shummshumm1231@gmail.com>
2021-02-23 10:28:22 +0900

    hoge

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ function require_auth() {
     $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
     $is_not_authenticated = (
         !$has_supplied_credentials ||
-        $_SERVER['PHP_AUTH_USER'] != $AUTH_USER = 'Yoshidame'; ||
-        $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS = 'PythonM2';
+        $_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
+        $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
     );
     if ($is_not_authenticated) {
         header('HTTP/1.1 401 Authorization Required');
